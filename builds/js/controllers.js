7d822127d98899a8a17066a34c97d0071cd3e1aa
AlfimovDev <kalfimov@gmail.com>
2016-11-13 23:23:51 +0200

    add feature for add and edit array

--- builds/js/controllers.js
+++ builds/js/controllers.js
@@ -1,5 +1,6 @@
 
 phoneBook.controller('phoneBookCtrl',function($scope){
+    $scope.flagEdit = false;
     $scope.phoneUsersList = [
         {
             firstName: "Alfimov",
@@ -7,18 +8,51 @@ phoneBook.controller('phoneBookCtrl',function($scope){
             group: "friends"
         }
     ];
+
+    
+
     $scope.addUsers = function () {
-        var elments = {
-            firstName: $scope.name,
-            avatar: $scope.avatar,
-            group: $scope.groupe
-        };
-        $scope.phoneUsersList.push(elments);
-        console.log($scope.phoneUsersList);
+        
+        if ($scope.flagEdit) {
+            var link_array = $scope.phoneUsersList[$scope.flagEdit];
+            link_array.firstName = $scope.name;
+            link_array.avatar = $scope.avatar;
+            link_array.group = $scope.groupe;
+            $scope.flagEdit = false;
+        } else {
+            var elments = {
+                firstName: $scope.name,
+                avatar: $scope.avatar,
+                group: $scope.groupe
+            };
+            $scope.phoneUsersList.push(elments);
+        }
+        
+        $scope.name = '';
+        $scope.avatar = '';
+        $scope.groupe = '';
     }
+
     $scope.removeUsers = function (i) {
-        console.log(i);
-        $scope.phoneUsersList.splice(1,1);
+        var x = searchByHashkey ( $scope.phoneUsersList, i);
+        $scope.phoneUsersList.splice(x,1);
+    }
+
+    $scope.editUsers = function (n) {
+        var x = searchByHashkey ( $scope.phoneUsersList, n);
+        var index = $scope.phoneUsersList[x];
+        $scope.name = index.firstName;
+        $scope.avatar = index.avatar;
+        $scope.groupe = index.group;
+        $scope.flagEdit = searchByHashkey ( $scope.phoneUsersList, n);
     }
     
+    function searchByHashkey (obj, key) {
+        for (var i in obj) {
+            if ( obj[i].$$hashKey == key ) {
+                return i;
+            }
+        }
+        return console.log("Not Found");
+    }
 });
